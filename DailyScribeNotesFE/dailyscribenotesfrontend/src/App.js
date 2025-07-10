98097b8b0109358444c9511b9d2e287c65511cef
Arnav Katyayan <ishumisra99@gmail.com>
2025-07-10 21:48:21 +0530

    Release notes and version no changes.

--- DailyScribeNotesFE/dailyscribenotesfrontend/src/App.js
+++ DailyScribeNotesFE/dailyscribenotesfrontend/src/App.js
@@ -152,7 +152,7 @@ function App() {
 
       <div className='footer-class'>
           <div className='version-author'>
-            <p className='version-no'>Version 1.0.0 | </p>
+            <p className='version-no'>Version 1.0.1 | </p>
             <p className='version-no'>Arnav Katyayan | </p>
             <p className='version-no'>All Rights Reserved </p>
           </div>
